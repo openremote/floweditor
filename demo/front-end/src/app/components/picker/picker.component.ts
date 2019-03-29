c1443a0265c8296167dbd0e6887b9304c6bfd1c9
Mestiez <m.pereira@student.fontys.nl>
2019-03-29 17:06:00 +0100

    translation

--- demo/front-end/src/app/components/picker/picker.component.ts
+++ demo/front-end/src/app/components/picker/picker.component.ts
@@ -19,8 +19,9 @@ export class PickerComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this.internal.picker.type == PickerType.Dropdown)
+    if (this.internal.picker.type == PickerType.Dropdown) {
       this.internal.value = this.internal.picker.options[0];
+    }
   }
 
 }
--- demo/front-end/src/app/models/translating/ServerReadyNode.ts
+++ demo/front-end/src/app/models/translating/ServerReadyNode.ts
@@ -1,17 +1,18 @@
-import { ServerReadyInternal } from "./ServerReadyInternal";
+import { ServerReadyInternal } from './ServerReadyInternal';
 import { GraphNode } from '../graphnode';
 export class ServerReadyNode {
     public type: string;
     public id: number;
     public internals: ServerReadyInternal[] = [];
 
-    constructor(node : GraphNode){
+    constructor(node: GraphNode) {
         this.type = node.type.toString();
         this.id = node.id;
         node.internals.forEach(internal => {
-            let srInternal : ServerReadyInternal = new ServerReadyInternal();
+            const srInternal: ServerReadyInternal = new ServerReadyInternal();
             srInternal.name = internal.name;
             srInternal.value = internal.value;
+            this.internals.push(srInternal);
         });
     }
 }
