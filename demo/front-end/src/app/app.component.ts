975cefd682f30bc48c9d59e10752e95163244bcb
Mestiez <m.pereira@student.fontys.nl>
2019-05-25 20:46:22 +0200

    some polishing

--- demo/front-end/src/app/app.component.ts
+++ demo/front-end/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import openremote, { Auth, Manager } from '@openremote/core';
+enum Status {
+  Busy,
+  Failure,
+  Authenticated,
+  Unauthenticated
+}
 
 @Component({
   selector: 'app-root',
@@ -8,8 +14,11 @@ import openremote, { Auth, Manager } from '@openremote/core';
 })
 export class AppComponent implements OnInit {
 
-  private editorVisible = false;
   private openremote = openremote;
+
+  private Status = Status;
+
+  private status = Status.Busy;
 
   constructor() {
 
@@ -21,7 +30,14 @@ export class AppComponent implements OnInit {
       keycloakUrl: 'http://localhost/auth',
       auth: Auth.KEYCLOAK,
       autoLogin: true,
-      realm: 'master'
-    }).then(() => this.editorVisible = true);
+      realm: 'tenantA'
+    }).then(
+      (result) => {
+        if (result) {
+          this.status = openremote.authenticated ? Status.Authenticated : Status.Unauthenticated;
+        } else {
+          this.status = Status.Failure;
+        }
+      });
   }
 }
