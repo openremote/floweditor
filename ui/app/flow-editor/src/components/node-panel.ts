80c6a6aa0e4afdc11223b6b2d56ab1a4dd207a85
mestiez <mestiez@outlook.com>
2019-10-14 11:27:40 +0200

    lit element project created

---
+++ ui/app/flow-editor/src/components/node-panel.ts
@@ -0,0 +1,15 @@
+import { LitElement, html, customElement, css } from "lit-element";
+
+@customElement("node-panel")
+export class NodePanel extends LitElement {
+    static get styles() {
+        return css`
+        `;
+    }
+
+    public render() {
+        return html`
+        nodepanel
+        `;
+    }
+}
---
+++ ui/app/flow-editor/src/index.ts
@@ -0,0 +1,1 @@
+export { NodePanel } from "./components/node-panel";
