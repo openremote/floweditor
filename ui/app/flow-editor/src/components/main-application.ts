97943077f9738ae2c53a22dc88ec25830d55d188
mestiez <32131519+mestiez@users.noreply.github.com>
2019-11-18 09:59:32 +0100

    support for translations

--- ui/app/flow-editor/src/components/main-application.ts
+++ ui/app/flow-editor/src/components/main-application.ts
@@ -4,6 +4,9 @@ import { Status } from "../models/status";
 
 @customElement("main-application")
 export class MainApplication extends LitElement {
+    @property({ type: Boolean }) public showHeader = true;
+    @property({ type: Boolean }) public showNodePanel = true;
+
     static get styles() {
         return css`
         :host{
@@ -28,11 +31,30 @@ export class MainApplication extends LitElement {
     protected render() {
         if (integration.status === Status.Idle || integration.status === Status.Loading) { return html``; }
 
+        if (this.showHeader) {
+            this.style.gridTemplateAreas = `
+            "topbar topbar"
+            "node-panel workspace"
+            `;
+        } else {
+            this.style.gridTemplateAreas = `
+            "node-panel workspace"
+            "node-panel workspace"
+            `;
+        }
+        
+        if (!this.showHeader) {
+            this.style.gridTemplateAreas = this.style.gridTemplateAreas.replace("topbar topbar", "node-panel workspace");
+        }
+        if (!this.showNodePanel) {
+            this.style.gridTemplateAreas = this.style.gridTemplateAreas.replace(/node-panel/g, "workspace");
+        }
+
         if (integration.status === Status.Success) {
             return html`
             <editor-workspace id="workspace" style="grid-area: workspace"></editor-workspace>
-            <node-panel style="grid-area: node-panel"></node-panel>
-            <top-bar style="grid-area: topbar"></top-bar>
+            ${(this.showNodePanel ? html`<node-panel style="grid-area: node-panel"></node-panel>` : null)}
+            ${(this.showHeader ? html`<top-bar style="grid-area: topbar"></top-bar>` : null)}
             <context-menu></context-menu>
             <popup-modal id="popup-modal"></popup-modal>
         `;
